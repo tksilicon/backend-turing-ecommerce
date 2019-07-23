27f1f89f3663a5e6600d31ea2e7179bb81a55c59
tksilicon <frankgod02@gmail.com>
2019-07-23 16:17:11 +0100

    update my app

--- src/main/java/com/turing/ecommerce/heroku/DatabaseConfig.java
+++ src/main/java/com/turing/ecommerce/heroku/DatabaseConfig.java
@@ -12,13 +12,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
-//@Configuration
+@Configuration
+@Primary
 public class DatabaseConfig {
 
-  //@Value("${spring.datasource.url}")
+  @Value("${spring.datasource.url}")
   private String dbUrl;
 
-  //@Bean
+  @Bean
   public DataSource dataSource() {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
