2f45074e3d7d6662fee9bdaf8f148a09f97ea105
tksilicon <frankgod02@gmail.com>
2019-07-31 03:45:01 +0100

    update

--- src/main/java/com/turing/ecommerce/DTO/ProductGetAllDTO.java
+++ src/main/java/com/turing/ecommerce/DTO/ProductGetAllDTO.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 public class ProductGetAllDTO {
 	@ApiModelProperty(example="20")
 	private String count;
-	@ApiModelProperty(dataType="com.turing.ecommerce.DTO.ProuctAll", name = "rows", example = "[ProductAll]")
+	@ApiModelProperty(dataType="List", value = "rows")
 	private List<ProductAll> rows;
 
 }
